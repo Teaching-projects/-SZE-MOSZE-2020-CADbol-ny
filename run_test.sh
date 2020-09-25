518d5afdb3377935c48be99d69b75fe67895ac59
dobaybalazs <dobay@LAPTOP-DRLG2LA9.localdomain>
2020-09-25 17:22:46 +0200

    Test#9

--- run_test.sh
+++ run_test.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-IFS=$'\n'
 
 rm output.txt
+IFS=$' '
 
-./a.out units/unit1.txt units/unit1.txt >> output.txt
-./a.out units/unit1.txt units/unit3.txt >> output.txt
-./a.out units/unit2.txt units/unit1.txt >> output.txt
-./a.out units/unit2.txt units/unit3.txt >> output.txt
-./a.out units/unit3.txt units/unit2.txt >> output.txt
-./a.out units/unit3.txt units/unit1.txt >> output.txt
+while read line line2 ; do
+./a.out $line $line2>> output.txt
+done < input.txt
