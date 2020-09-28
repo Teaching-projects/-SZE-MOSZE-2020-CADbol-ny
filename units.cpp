328f7cf74bc91ad805ea2c3c972c9d7f30d966f3
dobaybalazs <dobay@LAPTOP-DRLG2LA9.localdomain>
2020-09-28 21:36:53 +0200

    Complete required changes

--- units.cpp
+++ units.cpp
@@ -1,25 +1,29 @@
 #include "units.h"
+#include <fstream>
 
 Unit::Unit(std::string name, int hp, int damage) :m_name(name), m_hp(hp), m_damage(damage) {}
 
 void Unit::dealDamageTo(Unit& damagedUnit){
 	damagedUnit.m_hp = damagedUnit.getHp() - this->getDamage() < 0 ? 0 : damagedUnit.getHp() - this->getDamage();
-
 }
 
-void Unit::parseUnit(std::string fileName,Unit& unit){
+Unit* Unit::parseUnit(const std::string& fileName){
 	std::ifstream fileStream;
+	std::string name;
+	int hp, damage;
 	fileStream.open(fileName);
 	if(fileStream.fail()){
 		throw 1;
 	}
-	std::string line;
-	std::vector <std::string> lines;
-	while(std::getline(fileStream,line)){
-		lines.push_back(line);
+	std::string lines [5];
+	int i = 0;
+	while (std::getline(fileStream, lines[i])) {
+		i++;
 	}
-	unit.m_name = lines[1].substr(lines[1].find(':')+3, lines[1].find(',') - lines[1].find(':')-4);
-	unit.m_hp=std::stoi(lines[2].substr(lines[2].find(':') + 2, lines[2].find(',') - lines[1].find(':')));
-	unit.m_damage=std::stoi(lines[3].substr(lines[3].find(':') + 2, lines[3].size() - lines[1].find(':')));
+	name = lines[1].substr(lines[1].find(':') + 3, lines[1].find(',') - lines[1].find(':') - 4);
+	hp = std::stoi(lines[2].substr(lines[2].find(':') + 2, lines[2].find(',') - lines[1].find(':')));
+	damage = std::stoi(lines[3].substr(lines[3].find(':') + 2, lines[3].size() - lines[1].find(':')));
+	Unit* unit=new Unit(name, hp, damage);
 	fileStream.close();
+	return unit;
 }
--- units.h
+++ units.h
@@ -1,22 +1,18 @@
 #pragma once
 #include <string>
-#include <fstream>
-#include <iostream>
-#include <vector>
 
 class Unit {
 private:
-	mutable std::string m_name;
+	const std::string m_name;
 	int m_hp;
-	mutable int m_damage;
+	const int m_damage;
 public:
-	Unit():m_name(""),m_hp(0),m_damage(0) {};
 	Unit(std::string, int, int);
 	std::string getName() const { return m_name; }
 	int getHp() const { return m_hp; }
 	int getDamage() const { return m_damage; }
 	void dealDamageTo(Unit&);
-	static void parseUnit(std::string, Unit&);
+	static Unit* parseUnit(const std::string&);
 	~Unit() {}
 };
 
