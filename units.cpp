e9c846cd77bf5897824043d0713928e5353ab726
tupoljev <werja@icloud.com>
2020-10-15 21:36:12 +0200

    big fix

--- units.cpp
+++ units.cpp
@@ -1,10 +1,26 @@
 #include "units.h"
 #include <fstream>
 
-Unit::Unit(std::string name, int hp, int damage) :m_name(name), m_hp(hp), m_damage(damage) {}
+Unit::Unit(std::string name, int damage, int hp, float attackspeed) :m_name(name), m_damage(damage), m_hp(hp), m_attackspeed(attackspeed) {}
 
 void Unit::dealDamageTo(Unit& damagedUnit){
 	damagedUnit.m_hp = damagedUnit.getHp() - this->getDamage() < 0 ? 0 : damagedUnit.getHp() - this->getDamage();
+}
+
+void Unit::Fight(Unit* unit1, Unit* unit2) {
+	unit1->dealDamageTo(*unit2);
+	float nextattackunit1 = unit1->getAttackspeed();
+	float nextattackunit2 = unit2->getAttackspeed();
+	while (unit1->getHp() != 0 && unit2->getHp() != 0) {
+		if (nextattackunit1 <= nextattackunit2) {
+			unit1->dealDamageTo(*unit2);
+			nextattackunit1 += unit1->getAttackspeed();
+		}
+		else {
+			unit2->dealDamageTo(*unit1);
+			nextattackunit2 += unit2->getAttackspeed();
+		}
+	}
 }
 
 Unit* Unit::parseUnit(const std::string& fileName){
