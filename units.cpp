8aaa29e26cdc40c9199cf54971e45a4ee18323bf
tupoljev <werja@icloud.com>
2020-10-16 14:39:00 +0200

    attackcooldown fix#1

--- units.cpp
+++ units.cpp
@@ -32,14 +32,15 @@ Unit* Unit::parseUnit(const std::string& fileName){
 	if(fileStream.fail()){
 		throw 1;
 	}
-	std::string lines [5];
+	std::string lines [6];
 	int i = 0;
 	while (std::getline(fileStream, lines[i])) {
 		i++;
 	}
 	name = lines[1].substr(lines[1].find(':') + 3, lines[1].find(',') - lines[1].find(':') - 4);
 	hp = std::stoi(lines[2].substr(lines[2].find(':') + 2, lines[2].find(',') - lines[1].find(':')));
-	damage = std::stoi(lines[3].substr(lines[3].find(':') + 2, lines[3].size() - lines[1].find(':')));
+	damage = std::stoi(lines[3].substr(lines[3].find(':') + 2, lines[3].find(',') - lines[1].find(':')));
+	attackspeed = std::stof(lines[3].substr(lines[4].find(':') + 2, lines[3].size() - lines[1].find(':')));
 	Unit* unit=new Unit(name, hp, damage, attackspeed);
 	fileStream.close();
 	return unit;
